49f5c39b1d19d5db04827f148de07757d25438bd
abdullah-sameh <abdullahmaher@std.mans.edu.eg>
2023-01-19 10:29:06 +0200

    change lines

--- index2.js
+++ index2.js
@@ -17,8 +17,9 @@ function countDown(time) {
         if (time === 0) return clearInterval(myInterval)
         time -= 1
         let min = Math.floor(time / 60)
-        if (min < 10) min = '0' + min //* To display '00:00'
         let sec = time - (min * 60)
+        
+        if (min < 10) min = '0' + min //* To display '00:00'
         if (sec < 10) sec = '0' + sec //* To display '00:00'
         timeBoard.innerHTML = `${min}:${sec}`
         document.title = `${min}:${sec}`
@@ -28,12 +29,12 @@ function countDown(time) {
 function endTime(time) {
     const end = new Date(time)
     let hour = end.getHours()
+    let min = end.getMinutes()
+    let sec = end.getSeconds()
+
     if (hour > 12) hour -= 12 // to make system 12 o'clock
     if (hour < 10) hour = '0' + hour // '02'
-    console.log(hour)
-    let min = end.getMinutes()
     if (min < 10) min = '0' + min // '03'
-    let sec = end.getSeconds()
     if (sec < 10) sec = '0' + sec // '05'
 
     timeOut.innerText = `${hour}:${min}:${sec}`
